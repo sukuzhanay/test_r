be15c4776007703a87024a4e858809f5fb92dede
Christian Vladimir Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2022-09-23 08:47:33 +0200

    Added Hands on 01

---
+++ HANDS ON 01.R
@@ -0,0 +1,24 @@
+
+# ID SCRIPT ---------------------------------------------------------------
+
+## LENGUAJES DE PROGRAMACION ESTADISTICA
+## PROFESOR : CHRISTIAN SUCUZHANAY AREVALO 
+## ALUMNO : MIKE TYSON, EXP 21535220
+## HANDS ON 01
+
+
+# SHORCUTS ----------------------------------------------------------------
+## crtl + l = clean console
+## crtl + shift + r = new section
+
+
+# LOADING LIBS ------------------------------------------------------------
+
+
+# BASIC OPERATORS ---------------------------------------------------------
+
+# GETTING DATA FROM INTERNET  ---------------------------------------------
+
+# READING AND WRITING ( FILES ) -------------------------------------------
+
+
